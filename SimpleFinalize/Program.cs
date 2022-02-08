b68873383994a16dd77851fed237b18da17784cb
GreedNeSS <ruslan199126@gmail.com>
2022-02-09 00:00:57 +0300

    Fixed bug

--- SimpleFinalize/Program.cs
+++ SimpleFinalize/Program.cs
@@ -9,14 +9,21 @@ namespace SimpleFinalize
             ~MyResourceWrapper() => Console.Beep();
         }
 
+        public static void MyMethod()
+        {
+            MyResourceWrapper rw = new MyResourceWrapper();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Finalizers *****\n");
             Console.WriteLine("Hit the return key to shut down this app");
             Console.WriteLine("and force the GC to invoke Finalize()");
             Console.WriteLine("for finalizable objects created in this AppDomain.");
+            MyMethod();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
             Console.ReadLine();
-            MyResourceWrapper rw = new MyResourceWrapper();
         }
     }
 }

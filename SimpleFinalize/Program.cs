87a40ef90db9b92fa06e2e66927c01d592f18c9e
GreedNeSS <ruslan199126@gmail.com>
2022-02-08 23:57:48 +0300

    Added simple finalize example

---
+++ SimpleFinalize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleFinalize
+{
+    class Program
+    {
+        class MyResourceWrapper
+        {
+            ~MyResourceWrapper() => Console.Beep();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Finalizers *****\n");
+            Console.WriteLine("Hit the return key to shut down this app");
+            Console.WriteLine("and force the GC to invoke Finalize()");
+            Console.WriteLine("for finalizable objects created in this AppDomain.");
+            Console.ReadLine();
+            MyResourceWrapper rw = new MyResourceWrapper();
+        }
+    }
+}

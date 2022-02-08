4d3736c927fcc1cb44b2fc9ee8824cc4ed45d83c
GreedNeSS <ruslan199126@gmail.com>
2022-02-09 00:25:23 +0300

    Used IDisposable and Using

---
+++ SimpleDispose/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        class MyResourceWrapper : IDisposable
+        {
+            public void Dispose()
+            {
+                Console.WriteLine("***** Index Dispose *****!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            using(MyResourceWrapper mr1 = new(),
+                mr2 = new(), mr3 = new())
+            {
+                Console.WriteLine("Work");
+            }
+        }
+    }
+}

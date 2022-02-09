70fad198f56d5644a21b2213c79caefd44125c91
GreedNeSS <ruslan199126@gmail.com>
2022-02-09 19:03:29 +0300

    Use finalize and dispose

---
+++ FinalizableDisposableClass/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FinalizableDisposableClass
+{
+    class MyResourceWrapper : IDisposable
+    {
+        private bool isDispose = false;
+
+        private void CleanUp(bool disposing)
+        {
+            if (!isDispose)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Dispose");
+                }
+                Console.WriteLine("Clean up unmanaged resources here.");
+            }
+
+            isDispose = true;
+        }
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~MyResourceWrapper()
+        {
+            Console.Beep();
+            CleanUp(false);
+        }
+    }
+
+    class Program
+    {
+        static void MyMethod()
+        {
+            MyResourceWrapper resourceWrapper1 = new();
+            resourceWrapper1.Dispose();
+
+            MyResourceWrapper resourceWrapper2 = new();
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.ReadLine();
+        }
+    }
+}
